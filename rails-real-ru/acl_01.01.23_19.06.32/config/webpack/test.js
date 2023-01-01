be9a2f8be86e69b382131a83f19256d82f76b957
Kirill Khalevin <kirill.khalevin.hexlet@gmail.com>
2023-01-01 19:38:18 +0300

    destroy

--- rails-real-ru/acl_01.01.23_19.06.32/config/webpack/test.js
+++
@@ -1,8 +0,0 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
-const environment = require('./environment')
-
-const config = environment.toWebpackConfig()
-config.devtool = 'none'
-
-module.exports = config
--- rails-real-ru/acl_01.01.23_19.21.47/app/controllers/posts_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class PostsController < ApplicationController
-  after_action :verify_authorized, except: %i[index show]
-
-  # BEGIN
-  
-  # END
-end
