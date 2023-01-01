5fe10d8277f9079047133f7bde911436762ea5ca
@hexlet/cli <support@hexlet.io>
2023-01-01 19:44:06 +0300

    submit rails-real-ru/acl

--- rails-real-ru/acl/app/controllers/posts_controller.rb
+++ rails-real-ru/acl/app/controllers/posts_controller.rb
@@ -4,6 +4,62 @@ class PostsController < ApplicationController
   after_action :verify_authorized, except: %i[index show]
 
   # BEGIN
-  
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find params[:id]
+
+    @posts = @post.posts
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find params[:id]
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      sign_in @post
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @post = Post.find params[:id]
+
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find params[:id]
+
+    @post.destroy
+
+    redirect_to posts_url, notice: 'Post was successfully destroyed.'
+  end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(
+      :title,
+      :body,
+      :author_id
+    )
+  end
   # END
 end
