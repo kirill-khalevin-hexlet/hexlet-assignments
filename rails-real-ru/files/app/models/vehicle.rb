a7dc712c90c12f3bb7ca980edc78091ac29fcec7
@hexlet/cli <support@hexlet.io>
2023-01-01 21:29:02 +0300

    submit rails-real-ru/files

--- rails-real-ru/files/app/models/vehicle.rb
+++ rails-real-ru/files/app/models/vehicle.rb
@@ -3,7 +3,7 @@
 class Vehicle < ApplicationRecord
   # BEGIN
   has_one_attached :image
-  validate :image_size_validation
+  validates :image, file_size: { less_than_or_equal_to: 5.megabytes }
   # END
 
   validates :manufacture, presence: true
@@ -14,8 +14,5 @@ class Vehicle < ApplicationRecord
   validates :production_year, presence: true
 
   # BEGIN
-  def image_size_validation
-    errors[:image] << 'should be less than 5MB' if image.present? && image.size > 5.megabytes
-  end
   # END
 end
